2454ca415cff8e56e9a96dc6f1ba89917cdb0bdc
fujitakk <fujita_k@njc.co.jp>
2016-10-24 21:11:56 +0900

    ドロワー「お気に入り一覧」の表示/非表示のタイミングを変更

--- app/src/main/java/jp/techacademy/kaori/fujita/qa_app/MainActivity.java
+++ app/src/main/java/jp/techacademy/kaori/fujita/qa_app/MainActivity.java
@@ -158,24 +158,30 @@ public class MainActivity extends AppCompatActivity {
 
 		// ナビゲーションドロワーの設定
 		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, mToolbar, R.string.app_name, R.string.app_name);
+		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, mToolbar, R.string.app_name, R.string.app_name){
+			// 開いた時のイベント
+			public void onDrawerOpened(View drawerView) {
+				//お気に入りの表示非表示
+				NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+				Menu menu = navigationView.getMenu();
+				MenuItem item = menu.findItem(R.id.nav_favorite);
+				// ログイン済みのユーザーを取得する
+				FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+				// ログインしていなければ、ドロワーにお気に入り一覧を表示しない
+				if (user == null) {
+					item.setVisible(false);
+				} else {
+					item.setVisible(true);
+				}
+
+				invalidateOptionsMenu();
+			}
+
+		};
 		drawer.addDrawerListener(toggle);
 		toggle.syncState();
 
 		NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-
-		//お気に入りの表示非表示
-		Menu menu = navigationView.getMenu();
-		MenuItem item = menu.findItem(R.id.nav_favorite);
-		// ログイン済みのユーザーを取得する
-		FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-		// ログインしていなければ、ドロワーにお気に入り一覧を表示しない
-		if (user == null) {
-			item.setVisible(false);
-		} else {
-			item.setVisible(true);
-		}
-
 		navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
 			@Override
 			public boolean onNavigationItemSelected(MenuItem item) {
